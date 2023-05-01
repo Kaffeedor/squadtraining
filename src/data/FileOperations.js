935282a49c33cd06082918381ca6731b2e4203fb
Kaffeedor <kaffeedor@gmail.com>
2023-05-01 12:56:31 +0200

    Fixed FileOps and added UUID for rolelist msgs

--- src/data/FileOperations.js
+++ src/data/FileOperations.js
@@ -2,17 +2,33 @@ const fs = require('fs');
 
 module.exports = {
 	read0: function Read0(fileName) {
-		return JSON.parse(fs.readFileSync(fileName).toString());
+		return JSON.parse(fs.readFileSync(fileName));
 	},
-	write0: function Write0(fileName, messageID0, role_names0, author0) {
-		const obj = JSON.parse(fs.readFileSync(fileName).toString());
-		console.log(parseInt(obj.length));
-		const new_obj = new {};
-		new_obj.index = parseInt(parseInt(obj.slice(-1).index) + 1);
-		new_obj.messageID = messageID0.toString();
-		new_obj.role_names = role_names0;
-		new_obj.author = author0.toString();
-		obj['message' + (parseInt(obj.slice(-1).index) + 1).toString()] = new_obj;
+	appendRoleListMessage: function appendRoleListMessage(fileName, objectKey, messageID, roleNames, author) {
+		// Read the existing JSON data from the file
+		const jsonData = fs.readFileSync(fileName, 'utf8');
+		const obj = JSON.parse(jsonData);
+
+		// Find the highest index in the object
+		let highestIndex = -1;
+		for (const key in obj) {
+			const index = obj[key].index;
+			if (index > highestIndex) {
+				highestIndex = index;
+			}
+		}
+
+		// Create a new object with the next highest index and provided message data
+		const newIndex = highestIndex + 1;
+		const newObject = {
+			index: newIndex,
+			messageID: messageID,
+			role_names: roleNames,
+			author: author,
+		};
+
+		// Add the new object to the JSON data and write it to the file
+		obj[objectKey] = newObject;
 		fs.writeFileSync(fileName, JSON.stringify(obj));
 	},
 };
